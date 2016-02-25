249ebfab83b5987640fd66362e5c15e26aa4c4d6
DodoGTA <aidas957@gmail.com>
2016-02-25 17:58:37 +0200

    Remove useless lines

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -44,7 +44,6 @@
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/libwebviewchromium.so)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/libwebviewchromium.so)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/webview)
 
 # ******************************************************************
